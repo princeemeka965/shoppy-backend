5260ef48dcc677e66b2d9860039fac254f3ad387
princeemeka965 <anyanwue4@gmail.com>
2023-12-29 23:35:50 +0100

    validate if user exists

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,8 +4,14 @@ const createAccount = (async (req, res) => {
     const { name, email, username, password } = req.body;
 
     try {
-        const user = await User.create({ name, email, username, password });
-        return res.status(200).json({ message: 'Account Created Successfully', user })
+        const userData = await User.findAll({ where: { username: username } });
+        if (userData.length === 0) {
+            const user = await User.create({ name, email, username, password });
+            return res.status(200).json({ message: 'Account Created Successfully', user })
+        }
+        else {
+            return res.status(500).json({ message: 'User already exists' });
+        }
     }
     catch (error) {
         console.log(error)
